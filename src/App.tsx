718a08b39c91cc8c7890d1a4c0a43ff17f49f9b3
Teodor Ghinea <ionutteo972@gmail.com>
2022-11-25 00:08:48 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ function App() {
   return (
     <>
       <div className="App-header">
-        <div className="typed-out">QR-CITY</div>
-        <div className="subtitle">Made for people. In cities.</div>
+        <div>
+          <div className="typed-out">QR-CITY </div>
+          <div className="subtitle">This text will blink at cool people.</div>
+        </div>
       </div>
     </>
   );
