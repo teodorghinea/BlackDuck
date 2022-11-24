dc5fd33f8f30efdab23e2fe37f7c8ee9af471d30
Teodor Ghinea <ionutteo972@gmail.com>
2022-11-24 11:22:42 +0200

    .

--- scripts/Weather/weather.js
+++ scripts/Weather/weather.js
@@ -1,32 +1,30 @@
-
 const Http = new XMLHttpRequest();
 const randomFoxUrl = 'https://randomfox.ca/floof/';
 
-function getFoxPicture(elementId, resultId){
+function getFoxPicture(resultId) {
 
     var result;
+    var element = document.getElementById(resultId);
+    element.innerHTML = "Asteapta putin ..."
 
-    document.getElementById(elementId).onclick = (() => {
 
-        fetch(randomFoxUrl, 
-            {
-                method: "GET",
-            })
-            .catch((exception) => console.log("Exception!!:" + exception))
-            .then(
-                async (response) => {
-                    result = await response.json();
-                    console.log(JSON.stringify(result));
+    fetch(randomFoxUrl, {
+            method: "GET",
+        })
+        .catch((exception) => console.log("Exception!!:" + exception))
+        .then(
+            async (response) => {
 
-                    var element = document.getElementById(resultId);
-                    var img = document.createElement('img');
-                    img.src = result.image;
-                    element.innerHTML = "";
-                    element.appendChild(img);
-                }
-            )
-        }
-    );
+                element.innerHTML = "Asteapta putin ..."
+
+                result = await response.json();
+                console.log(JSON.stringify(result));
+
+                var img = document.createElement('img');
+                img.src = result.image;
+
+                element.innerHTML = null;
+                element.appendChild(img);       
+            }
+        )
 }
-
-
