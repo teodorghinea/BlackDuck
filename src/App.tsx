58bb1a5398a505fd533f2f8840c6c8cc5d0d4f78
Teodor Ghinea <ionutteo972@gmail.com>
2022-11-24 23:42:20 +0200

    Changes to welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Textarea } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">QR-CITY</header>
-    </div>
+    <>
+      <div className="App-header">
+        <div className="typed-out">QR-CITY</div>
+        <div className="subtitle">Made for people. In cities.</div>
+      </div>
+    </>
   );
 }
 
